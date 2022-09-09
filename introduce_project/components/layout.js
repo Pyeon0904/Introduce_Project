a068c3c566c70578db128fa66f08e09dac6e9356
Onestar <jjp2548@naver.com>
2022-09-09 17:41:30 +0900

    [Next_레이아웃] 공통영역

--- introduce_project/components/layout.js
+++ introduce_project/components/layout.js
@@ -7,7 +7,6 @@ export default function Layout({ children }) {
   return(
     <>
       <Header />
-      <h1>레이아웃</h1>
     {/* 외부에서 들어오는 요소를 넣어줄 때에는 {} 중괄호 사용 */}
       <div>{children}</div>
       <Footer />
